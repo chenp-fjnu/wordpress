b524739dc8d995fe0f39d5e6c2ceefdd0d32afa5
chenp-fjnu <chp0524@gmail.com>
2016-08-24 01:38:01 +0800

    update comment look and feel

--- wp-includes/class-walker-comment.php
+++ wp-includes/class-walker-comment.php
@@ -323,21 +323,19 @@ class Walker_Comment extends Walker {
 		<<?php echo $tag; ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $this->has_children ? 'parent' : '', $comment ); ?>>
 			<article id="div-comment-<?php comment_ID(); ?>" class="comment-body">
 				<footer class="comment-meta">
-					<div class="comment-author vcard">
-						<?php if ( 0 != $args['avatar_size'] ) echo get_avatar( $comment, $args['avatar_size'] ); ?>
-						<?php printf( __( '%s <span class="says">says:</span>' ), sprintf( '<b class="fn">%s</b>', get_comment_author_link( $comment ) ) ); ?>
-					</div><!-- .comment-author -->
-
 					<div class="comment-metadata">
-						<a href="<?php echo esc_url( get_comment_link( $comment, $args ) ); ?>">
-							<time datetime="<?php comment_time( 'c' ); ?>">
+						<time datetime="<?php comment_time( 'c' ); ?>">
 								<?php
 									/* translators: 1: comment date, 2: comment time */
-									printf( __( '%1$s at %2$s' ), get_comment_date( '', $comment ), get_comment_time() );
+									printf( __( '%1$s at %2$s' ), get_comment_date( '', $comment ), get_comment_time() ).comment_reply_link( array_merge( $args, array(
+                    'add_below' => 'div-comment',
+                    'depth'     => $depth,
+                    'max_depth' => $args['max_depth'],
+                    'before'    => '<div class="reply">',
+                    'after'     => '</div>'
+                ) ) );
 								?>
 							</time>
-						</a>
-						<?php edit_comment_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
 					</div><!-- .comment-metadata -->
 
 					<?php if ( '0' == $comment->comment_approved ) : ?>
@@ -349,15 +347,6 @@ class Walker_Comment extends Walker {
 					<?php comment_text(); ?>
 				</div><!-- .comment-content -->
 
-				<?php
-				comment_reply_link( array_merge( $args, array(
-					'add_below' => 'div-comment',
-					'depth'     => $depth,
-					'max_depth' => $args['max_depth'],
-					'before'    => '<div class="reply">',
-					'after'     => '</div>'
-				) ) );
-				?>
 			</article><!-- .comment-body -->
 <?php
 	}
